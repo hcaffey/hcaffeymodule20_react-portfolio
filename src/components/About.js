1ca32fa5c76b27c2795b0d21f2301bee61987c38
Hayden Caffey <hayden@Haydens-MacBook-Pro-2.local>
2023-08-03 19:23:45 -0400

    current state

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div className="flex justify-content-center flex-column align-items">
+      <div className= "border-box flex justify-content-center flex-column align-items">
+        <div className="border-box">
+      </div>
+      <div className="border-box">
+        <p className="text-align color-blue">
+        Bio  
+        </p>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = ()=> {
+    return(
+        <div className="flex justify-content-center margin-bottom">
+        <div className="border-box ">
+            <footer>
+                <ul className="border-box flex space-around list-style-type-none">
+                    
+                </ul>
+            </footer>
+        </div>
+        </div>
+    )
+};
+
+export default Footer;
